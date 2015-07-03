1ef70b130901aee142590c92803ba4d38614cfd6
colin-h <colinharman@utexas.edu>
2015-07-03 15:49:09 -0500

    reduced foundation components, changed color scheme

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+// $(document).ready(function(){
+//   $('.panel').fadeIn("fast");
+//   console.log($('.panel'));
+// });
+
+// $('.profile').on("mouseover", function(){
+//   $(this).css("border-color", "#fff");
+// });
